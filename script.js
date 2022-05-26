66208f47bdf93bf70bf7c17cb6f7f0db71908c1f
antoniocns <acnspeixe@gmail.com>
2022-05-26 20:19:48 -0300

    adiciona resolução do exercicio

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = prompt("digite seu nome:");//wellington
+let sobrenome = prompt("digite seu sobre nome:");
+
+alert("nome completo: "+nome+" " +sobrenome);// "STRING" FRASE TEXTO LITERAL /'STRING' `TEMPLATE ${} ${} ${NOME}`
+
+
+alert(`nome de catalago: ${sobrenome.toUpperCase()} ,${nome}`);
+
+alert(`nome = ${sobrenome.toUpperCase()},sobrenome = ${nome.toUpperCase()}`);
